1f170a76b901d005b2c8697a268fd31d7a06aad3
hyacheungac <ache706@aucklanduni.ac.nz>
2020-09-04 21:54:00 +1200

    Commit attempt

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -9,14 +9,48 @@ class MovieFileCSVReader:
 
     def __init__(self, file_name: str):
         self.__file_name = file_name
+        self.__dataset_of_movies = []
+        self.__dataset_of_actors = set()
+        self.__dataset_of_directors = set()
+        self.__dataset_of_genres = set()
 
     def read_csv_file(self):
-        with open(self.__file_name, mode='r', encoding='utf-8-sig') as csvfile:
-            movie_file_reader = csv.DictReader(csvfile)
+        with open(self.__file_name, mode='r', encoding='utf-8-sig') as csv_file:
+            movie_file_reader = csv.DictReader(csv_file)
 
-            index = 0
             for row in movie_file_reader:
                 title = row['Title']
                 release_year = int(row['Year'])
-                print(f"Movie {index} with title: {title}, release year {release_year}")
-                index += 1
+                description = row["Description"]
+                runtime_minutes = row["Runtime (Minutes)"]
+                director = Director(row["Director"])
+                genres = set([Genre(genre) for genre in row["Genre"].split(',')])
+                actors = set([Actor(name) for name in row["Actors"].split(',')])
+
+                movie = Movie(title, release_year)
+                movie.description = description
+                movie.runtime_minutes = runtime_minutes
+                movie.director = director
+                movie.genre = genres
+                movie.actor = actors
+
+                self.__dataset_of_directors.add(director)
+                self.__dataset_of_genres |= genres
+                self.__dataset_of_actors |= actors
+                self.__dataset_of_movies.append(movie)
+
+    @property
+    def dataset_of_movies(self):
+        return self.__dataset_of_movies
+
+    @property
+    def dataset_of_actors(self):
+        return self.__dataset_of_actors
+
+    @property
+    def dataset_of_directors(self):
+        return self.__dataset_of_directors
+
+    @property
+    def dataset_of_genres(self):
+        return self.__dataset_of_genres
--- domainmodel/director.py
+++ domainmodel/director.py
@@ -1,4 +1,3 @@
-
 class Director:
 
     def __init__(self, director_full_name: str):
@@ -15,24 +14,10 @@ class Director:
         return f"<Director {self.__director_full_name}>"
 
     def __eq__(self, other):
-        # TODO
-        pass
+        return self.__director_full_name == other.__director_full_name
 
     def __lt__(self, other):
-        # TODO
-        pass
+        return self.__director_full_name < other.__director_full_name
 
     def __hash__(self):
-        # TODO
-        pass
-
-
-class TestDirectorMethods:
-
-    def test_init(self):
-        director1 = Director("Taika Waititi")
-        assert repr(director1) == "<Director Taika Waititi>"
-        director2 = Director("")
-        assert director2.director_full_name is None
-        director3 = Director(42)
-        assert director3.director_full_name is None
+        return hash(self.__director_full_name)
